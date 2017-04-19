8623f198dc5747dd2226408e3a062fc11343ab6e
Marc Bernstein <marc.bernstein@hudl.com>
2017-04-19 16:35:58 -0500

    Dynamic rendering

---
+++ app/components/Results.js
@@ -0,0 +1,16 @@
+var React = require('react');
+//var PropTypes = require('prop-types');
+//var Link = require('react-router-dom').Link;
+
+class Results extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        Results
+      </div>
+    )
+  }
+}
+
+module.exports = Results;
